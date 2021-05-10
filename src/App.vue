6af020056f157500bd74d58535394566a3e727fb
Francesco Falleni <56653917+Checco9811@users.noreply.github.com>
2021-05-10 23:57:14 +0200

    Creating skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
   <div id="app">
+    <b-container>
+      <b-row>
+        <b-col>
+          <b-row>
+            <!--Map here-->
+          </b-row>
+
+          <b-row>
+
+          </b-row>
+
+        </b-col>
+        <b-col cols="3">
+          <b-form-group label="Select a CarId" id="carList">
+            <b-form-checkbox-group
+                size="lg"
+                v-model="CarID.value"
+                :options="CarID.options"
+                name="buttonsCarId"
+                buttons
+                stacked
+                style="width: 100%"
+            ></b-form-checkbox-group>
+          </b-form-group>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -8,6 +35,15 @@
 export default {
   name: 'App',
   components: {
+  },
+  data(){
+    return{
+      CarID: {
+        value: [1,2],
+        options: [1,2,3],
+      }
+    }
+
   }
 }
 </script>
